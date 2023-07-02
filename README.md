210328d74e3b60195cce4e617c1d158fbda0310d
morvo <maciejkuchta5@gmail.com>
2023-07-02 19:07:48 +0200

    Major changes. Added loops.

--- README.md
+++ README.md
@@ -16,9 +16,9 @@ There you go, you just built the project.
 Next step is to run it. To do so put this into your terminal
 
 ```
-$ ./imvuck
+$ ./imvuck filename
 ```
-Now, put your code after the prompt. Imvuck will interprete it.
+Imvuck will interprete the code from your file.
 
 ## How to prorgram in it?
 
@@ -29,13 +29,14 @@ These are the characters you can use in imvuck:
 - `+`, this character increases the value of the array cell the pointer is pointing to.
 - `-`, this character decreases the value of the array cell the pointer is pointing to.
 - `.`, this character prints the value of the array cell in char form to which the pointer is pointing.
-- `!`, this character prints out the entire array as a string.
+- `[`, this character starts the loop. Jump to matching `]` if the value of the array cell the pointer is pointing to is zero,
+- `]`, this character ends the loop. Jump to matching `[` if the value of the array cell the pointer is pointing to is not zero,
 
 ## Have fun using my interpreter!
 ---
 # TODO
 - [X] `>` and `<` characters.
 - [X] `+` and `-` characters.
-- [X] `.` and `!` characters.
-- [ ] `[` and `]` characters - loops.
+- [X] `.` character.
+- [X] `[` and `]` characters - loops.
 - [ ] `,` character.
--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,104 @@
 #include <iostream>
+#include <fstream>
 
-char array[1'000'000] { 0 };
-char* pointer = array;
+struct Environment  {
+    char array[1'000'000] { 0 };
+    char* cell = array;
+    char* instruction = 0;
+};
 
-int interpret(std::string code) {
-    for (char c : code) {
-        switch (c) {
-            case '<':
-                if (pointer == array) return -2;
-                pointer--;
+void go_to_closing_bracket(Environment &env) {
+    int balance = 1;
+    do {
+        env.instruction++;
+        if (*env.instruction == '[') {
+            balance++;
+        }
+        else if (*env.instruction == ']') {
+            balance--;
+        }
+    } while (balance != 0);
+}
+
+void go_to_opening_bracket(Environment &env) {
+    int balance = 0;
+    do {
+        if (*env.instruction == '[') {
+            balance++;
+        }
+        else if (*env.instruction == ']') {
+            balance--;
+        }
+        env.instruction--;
+    } while (balance != 0);
+}
+
+int interpret(std::string code, Environment &env) {
+    if (code == "") return -2;
+    
+    for (env.instruction = code.data(); env.instruction < &code[code.length()-1]; env.instruction++) {
+        switch (*env.instruction) {
+            case '<': 
+                if (env.cell == env.array) return -1;
+                env.cell--;
                 break;
             case '>':
-                if (pointer == &array[1'000'000]) return -1;
-                pointer++;
+                if (env.cell == &env.array[1'000'000]) return -1;
+                env.cell++;
                 break;
             case '+':
-                (*pointer)++;
+                (*env.cell)++;
                 break;
             case '-':
-                (*pointer)--;
+                (*env.cell)--;
                 break;
             case '.':
-                std::cout << *pointer;
+                std::cout << *env.cell;
                 break;
-            case '!':
-                for (char ch : array) {
-                    std::cout << ch;
-                }
+            case '[':
+                if (*env.cell == 0) go_to_closing_bracket(env);
                 break;
+            case ']':
+                if (!*env.cell == 0) go_to_opening_bracket(env);
+                break;
+
         }
     }
     return 0;
 }
 
-int main() {
-    std::string code;
-    std::cout << "> ";
-    std::getline(std::cin, code);
-    int exit_status = interpret(code);
-    std::cout << std::endl;
-    if (exit_status == 0) {
-        return 0;
+int main(int argc, char** argv) {
+    if (argc <= 1) {
+        std::cout << "Usage: `./imvuck filename`" << std::endl;
+        return -3;
     }
-    std::cout << exit_status << std::endl;
+
+    Environment env;
+    std::string line;
+    std::string code;
+    std::ifstream file; 
+    file.open(argv[1]);
+
+    if (!file.is_open()) {
+        std::cout << "Error while reading the file." << std::endl;
+        return -4;
+    }
+
+    while (std::getline(file, line)){
+        code += line;
+    }
+
+    int exit_status = interpret(code, env);
+    std::cout << std::endl;
+    
+    switch (exit_status) {
+        case 0:
+            return 0;
+        case -1:
+            std::cout << "Index out of range." << std::endl;
+            return -1;
+        case -2:
+            std::cout << "Empty file." << std::endl;
+            return -2;
+    }
 }
